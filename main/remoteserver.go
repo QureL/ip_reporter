a849393aab0ed197ade6acaa241e34a043d90244
qurel <jlu_qurel@qq.com>
2021-11-21 15:44:17 +0800

    Sun Nov 21 15:44:17 CST 2021

--- main/remoteserver.go
+++ main/remoteserver.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
 	remoteserver "ip_reporter/remoteServer"
 	"os"
 	"strconv"
 )
 
 func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("input %s listen_port outputPath.\n", os.Args[0])
+		return
+	}
 	port, err := strconv.Atoi(os.Args[1])
 	if err != nil {
 		return
--- main/reporter.go
+++ main/reporter.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"ip_reporter/reporter"
 	"os"
 	"strconv"
 )
 
 func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("input %s remote_ip remote_port.\n", os.Args[0])
+		return
+
+	}
 	port, err := strconv.Atoi(os.Args[2])
 	if err != nil {
 		return
